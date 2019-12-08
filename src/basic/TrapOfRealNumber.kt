29fdbeb624ea14237afa3052755ca782caeec908
VenusPark <devenus2@gmail.com>
2019-12-08 18:06:34 +0900

    [Kotlin 200 Example] 009

---
+++ src/basic/TrapOfRealNumber.kt
@@ -0,0 +1,11 @@
+package basic
+
+/**
+ * 실수의 연산시 오차발생
+ */
+fun main(args: Array<String>){
+
+    println(0.1f + 0.1f + 0.1f)
+    println(0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f )
+    println(0.1f * 10)
+}
