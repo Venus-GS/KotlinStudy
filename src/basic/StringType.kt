bc5579a7b00784246a9f529880539d8d7c3a1085
VenusPark <devenus2@gmail.com>
2019-12-08 18:17:45 +0900

    [Kotlin 200 Example] 011

---
+++ src/basic/StringType.kt
@@ -0,0 +1,15 @@
+package basic
+
+fun main(args: Array<String>) {
+
+    var str: String = "Hello";
+    println(str)
+
+    str = str + "\nKotlin!"
+    println(str)
+
+    println(str[8])
+
+    var num = 10 * 5 +3
+    println(str + num)
+}
