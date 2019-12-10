fdb41d3ed761f46e8d3b29fc725830b9800a1c93
VenusPark <devenus2@gmail.com>
2019-12-10 19:17:04 +0900

    [Kotlin 200 Example] 021

---
+++ src/basic/NestedIfElse.kt
@@ -0,0 +1,18 @@
+package basic
+
+fun main(args: Array<String>){
+
+    val score = 88
+
+    if(score >= 90) println('A')
+
+    else{
+        if (score >= 80) println('B')
+
+        else{
+            if (score >= 70) println('C')
+
+            else println('F')
+        }
+    }
+}
