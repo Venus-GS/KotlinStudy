641a05590bb24a01210077a161458f8f4187f543
VenusPark <devenus2@gmail.com>
2019-12-10 23:02:08 +0900

    [Kotlin 200 Example] 040

---
+++ src/basic/MemoryStack.kt
@@ -0,0 +1,14 @@
+package basic
+
+fun main(args: Array<String>){
+
+    val a = -36
+    val result = absolute(a)
+    println(result)
+}
+
+fun absolute(number: Int): Int{
+
+    return  if(number >= 0) number
+            else -number
+}
